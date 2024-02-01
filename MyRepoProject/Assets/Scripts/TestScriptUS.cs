71d9b04ccd8271eb2deabfbaf621b35d43056304
Nicomedes443 <dusti107@canton.edu>
2024-01-31 23:56:24 -0500

    New Script TestScriptUS

---
+++ MyRepoProject/Assets/Scripts/TestScriptUS.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScriptUS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("HELLO FROM US!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
